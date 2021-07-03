4082a6f902825b8a437112071d4332a33e7651f4
grzeg <grzegorz.sowa314@gmail.com>
2021-07-03 15:07:19 +0200

    n+1 problem fixed with JPQL @Query

--- src/main/java/pl/recompiled/hibernatenplusonedemo/ShopOrderRepository.java
+++ src/main/java/pl/recompiled/hibernatenplusonedemo/ShopOrderRepository.java
@@ -1,5 +1,6 @@
 package pl.recompiled.hibernatenplusonedemo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
@@ -7,6 +8,10 @@ import java.util.UUID;
 
 interface ShopOrderRepository extends CrudRepository<ShopOrder, Long> {
 
+    @Query("""
+            SELECT o FROM ShopOrder o
+            LEFT JOIN FETCH o.positions
+            """)
     Set<ShopOrder> findAllByClientId(UUID clientId);
 
 }
