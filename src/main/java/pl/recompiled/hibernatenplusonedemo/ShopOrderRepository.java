c0a0f186368b75c19f6157d35ce1f3400f7f4dae
grzeg <grzegorz.sowa314@gmail.com>
2021-07-03 14:44:32 +0200

    n+1 problem fixed with @EntityGraph

--- src/main/java/pl/recompiled/hibernatenplusonedemo/ShopOrderRepository.java
+++ src/main/java/pl/recompiled/hibernatenplusonedemo/ShopOrderRepository.java
@@ -1,5 +1,6 @@
 package pl.recompiled.hibernatenplusonedemo;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
@@ -7,6 +8,7 @@ import java.util.UUID;
 
 interface ShopOrderRepository extends CrudRepository<ShopOrder, Long> {
 
+    @EntityGraph(attributePaths = {"positions"})
     Set<ShopOrder> findAllByClientId(UUID clientId);
 
 }
